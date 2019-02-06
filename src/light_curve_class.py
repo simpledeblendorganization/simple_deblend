060b2afc5ed4d09ae7b2fc2cba1d531dca30ede8
Joshua <joshuawallace800@gmail.com>
2019-02-06 18:57:50 -0500

    created src directory

--- src/light_curve_class.py
+++ src/light_curve_class.py
@@ -1,4 +1,4 @@
-'''lightcurve_class.py - Joshua Wallace - Jan 2019
+'''light_curve_class.py - Joshua Wallace - Feb 2019
 
 This contains a class useful for organizing an object's light curve and 
 other useful data, just to keep things clean going forward.
@@ -13,7 +13,7 @@ class light_curve():
             raise RuntimeError("mags does not have a len attribute")
         if not hasattr(errs_,'__len__'):
             raise RuntimeError("errs does not have a len attribute")
-        if len(times_) != lens(mags_):
+        if len(times_) != len(mags_):
             raise RuntimeError("The lengths of times and mags are not the same")
         if len(mags_) != len(errs_):
             raise RuntimeError("The lengths of mags and errs are not the same")
@@ -24,7 +24,7 @@ class light_curve():
 
 class single_lc_object(light_curve):
     def __init__(self,times_,mags_,errs_,x_,y_,ID_,extra_info_={}):
-        light_curve.init(self,times_,mags_,errs_)
+        light_curve.__init__(self,times_,mags_,errs_)
         self.x = x_
         self.y = y_
         self.ID = ID_
@@ -34,16 +34,20 @@ class single_lc_object(light_curve):
 
 class lc_objects():
     def __init__(self,radius_):
-        self.neighbor_radius = radius_
+        self.neighbor_radius_squared = radius_**2
         self.objects = []
+        self.index_dict = {}
 
     def add_object(self,object):
         if not isinstance(object,single_lc_object):
             raise RuntimeError("Was not given an instance of single_lc_object")
+        if object.ID in self.index_dict.keys():
+            raise RuntimeError("Not a unique ID, " + str(object.ID))
+        self.index_dict[object.ID] = len(self.objects)
 
         # Check if the new object is neighbor to any other objects
         for o in self.objects:
-            if (object.x - o.x)**2 + (object.y - o.y)**2 < self.neighbor_radius:
+            if (object.x - o.x)**2 + (object.y - o.y)**2 < self.neighbor_radius_squared:
                 object.neighbors.append(o.ID)
                 o.neighbors.append(object.ID)
 
