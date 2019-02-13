5449cf3687d395e821720466e84c4de624fb8b68
Joshua <joshuawallace800@gmail.com>
2019-02-13 15:20:19 -0500

    initialized test_data_processing.py

---
+++ test/test_data_processing.py
@@ -0,0 +1,11 @@
+'''test_data_processing.py - Joshua Wallace - Feb 2019
+
+This tests classes and methods in the data_processing.py file.
+'''
+
+import unittest
+import sys, os
+sys.path.insert(1,os.path.abspath('../src'))
+print(os.path.abspath('../src'))
+import data_processing as dproc
+
