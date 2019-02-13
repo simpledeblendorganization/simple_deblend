b3ab3c4a3b984dc10077c0aa4e59352c973b089c
Joshua <joshuawallace800@gmail.com>
2019-02-13 16:44:09 -0500

    first test to get travis running

--- test/test_data_processing.py
+++ test/test_data_processing.py
@@ -9,3 +9,10 @@ sys.path.insert(1,os.path.abspath('../src'))
 print(os.path.abspath('../src'))
 import data_processing as dproc
 
+
+class test_data_processing(unittest.TestCase):
+
+    # Test initialization of the lc_collection_for_processing class:
+    self.assertIsInstance(dproc.lc_collection_for_processing(1.),
+                              dproc.lc_collection_for_processing)
+
