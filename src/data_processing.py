826e7f3a35d708b350b595af01da7dc55e286e85
Joshua <joshuawallace800@gmail.com>
2019-02-13 20:05:36 -0500

    backup, halfway done data_process.py makeover

--- src/data_processing.py
+++ src/data_processing.py
@@ -7,7 +7,12 @@ data actually gets fed in and results actually get calculated.
 from light_curve_class import lc_objects
 import simple_deblend
 from multiprocessing import Pool, cpu_count
-from astrobase.periodbase.zgls import pgen_lsp
+from astrobase.periodbase.zgls import pgen_lsp #as gls_lsp
+from astrobase.periodbase.spdm import stellingwerf_pdm #as pdm_lsp
+from astrobase.periodbase.kbls import bls_parallel_pfind #as bls_lsp
+
+
+
 
 
 class lc_collection_for_processing(lc_objects):
@@ -22,7 +27,8 @@ class lc_collection_for_processing(lc_objects):
     radius   - the circular radius, in pixels, for objects to be in 
                sufficient proximity to be regarded as neighbors
     nworkers - (optional; default None) the number of workers to use in the
-               parallel calculation
+               parallel calculation---value of None defaults to 
+               multiprocessing.cpu_count
     '''
     def __init__(self,radius_,nworkers_=None):
         lc_objects.__init__(radius_)
@@ -30,6 +36,73 @@ class lc_collection_for_processing(lc_objects):
             self.nworkers = cpu_count
         else:
             self.nworkers = nworkers_
+        self._acceptable_methods = ['LS','BLS','PDM']
+
+
+    def run(self,which_methods,startp=None,endp=None,autofreq=True,
+                nbestpeaks=3,periodepsilon=0.1,stepsize=1.0e-4,
+                sigclip=float('inf'),output_dir="."):
+
+        # Check and make sure the methods are all acceptable ones
+        for method in which_methods:
+            if method.upper() is not in self._acceptable_methods:
+                raise ValueError(method.upper() + " is not one of "+\
+                                    "the accepted methods: " +\
+                                    str(self._acceptable_methods))
+
+        params = {startp:startp,endp:endp,autofreq:autofreq,
+                      nbestpeaks:nbestpeaks,periodepsilon:periodepsilon,
+                      stepsize:stepsize,sigclip:sigclip}
+
+        
+        mp_pool = Pool(self.nworkers)
+
+        _ = pool.starmap(self._run_single_object, [(o,params,which_methods,output_dir)
+                                                       for o in self.objects])
+
+    def _run_single_object(self,object,parms,methods,output_dir):
+
+        # Actually, just try John's function
+        neighbor_lightcurves = [(self.lc_objects.objects[self.lc_objects.objects.index_dict[neighbor_ID]].times,
+                                     self.lc_objects.objects[self.lc_objects.objects.index_dict[neighbor_ID]].mags,
+                                     self.lc_objects.objects[self.lc_objects.objects.index_dict[neighbor_ID]].errs) for neighbor_ID in object.neighbors]
+
+        all_results = {}
+
+        for method in methods:
+
+            #method_func_mapper = {
+            #    'ls':
+
+            results_storage = periodsearch_results(object.ID)
+
+            for _ in range(parms['nbestpeaks']):
+                rv = iterative_deblend(object.times,object.mags,object.errs,
+                                        neighbor_lightcurves,pgen,
+                                        results_storage,
+                                        function_parms=parms,
+                                        nharmonics_fit=7,
+                                        max_fap=.5,minimum_freq=1./parms['endp'],
+                                        maximum_freq=1./parms['startp'])
+                if rv is None:
+                        break
+
+            if len(results_storage.good_periods_info) > 0:
+                all_results[method] = results_storage
+            else:
+                all_results[method] = None
+
+        if not all
+        with open(output_dir + "periodsearch_" + object.ID + ".pkl") as f:
+            pickle.dump(results_storage,f)
+
+        ###### So what kind of information do I want returned?
+          # LSP of any well-found peak
+          # Record of which periods (and epochs) were blends
+          # Fourier-fitted lc's
+        
+
+            
 
 class periodsearch_results():
     def __init__(self,ID_):
@@ -51,79 +124,3 @@ class periodsearch_results():
         self.blend_info.append(dict_to_add)
 
 
-    def _lomb_scargle_single_object(self,info_passed_in,output_dir):
-        object = info_passed_in[0]
-        parms = info_passed_in[1]
-        # Run GLS on this object
-        #initial_lsp = pgen_lsp(object.times,object.mags,object.errs,
-        #                           startp=parms['startp'],endp=parms['endp'],
-        #                           autofreq=parms['autofreq'],nbestpeaks=1,
-        #                           periodepsilon=parms['periodepsilon'],
-        #                           stepsize=parms['stepsize'],
-        #                           nworkers=1,sigclip=parms['sigclip'],
-        #                           verbose=False)
-
-        # Compute FAP on the periods (just best period?)
-        # Question for John: what kind of frequency?  Hertz or angular frequency?
-        # Question for John: is GLS d_k=1? And what should d_H be?
-        #fap = simple_deblend(object.times,object.errs,initial_lsp['lspvals'],
-        #                         1/initial_lsp['startp'],d_K=1)
-                                                                
-
-        # Fit Fourier series to get flux amplitude
-
-        # Fit Fourier series for all neighbors to determine their
-        # flux amplitudes
-
-
-        # Compare flux amplitudes
-
-
-        # If neighbor has larger flux amplitude,
-        # mark it as such and rerun with the residual LC
-
-
-
-        ####
-        # Actually, just try John's function
-        neighbor_lightcurves = [(self.lc_objects.objects[self.lc_objects.objects.index_dict[neighbor_ID]].times,
-                                     self.lc_objects.objects[self.lc_objects.objects.index_dict[neighbor_ID]].mags,
-                                     self.lc_objects.objects[self.lc_objects.objects.index_dict[neighbor_ID]].errs) for neighbor_ID in object.neighbors]
-
-        results_storage = periodsearch_results(object.ID)
-
-        for _ in range(parms['nbestpeaks']):
-            rv = iterative_deblend(object.times,object.mags,object.errs,
-                                    neighbor_lightcurves,pgen,
-                                    results_storage,
-                                    function_parms=parms,
-                                    nharmonics_fit=7,
-                                    max_fap=.5,minimum_freq=1./parms['endp'],
-                                    maximum_freq=1./parms['startp'])
-            if rv is None:
-                    break
-
-        if len(results_storage.good_periods_info) > 0:
-            with open(output_dir + "periodsearch_" + object.ID + ".pkl") as f:
-                pickle.dump(results_storage,f)
-
-        ###### So what kind of information do I want returned?
-          # LSP of any well-found peak
-          # Record of which periods (and epochs) were blends
-          # Fourier-fitted lc's
-        
-
-    def lomb_scargle_run(self,startp=None,endp=None,autofreq=True,
-                             nbestpeaks=3,periodepsilon=0.1,stepsize=1.0e-4,
-                             sigclip=float('inf'),output_dir="."):
-
-        params = {startp:startp,endp:endp,autofreq:autofreq,
-                      nbestpeaks:nbestpeaks,periodepsilon:periodepsilon,
-                      stepsize:stepsize,sigclip:sigclip}
-
-        
-        mp_pool = Pool(self.nworkers)
-
-        _ = pool.map(_lomb_scargle_single_object, [(o,params,output_dir)
-                                                       for o in self.objects])
-        
