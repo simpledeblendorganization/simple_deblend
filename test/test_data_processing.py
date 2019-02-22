07a749f0dcaa7b275bb4eb505caf2d867d226f4b
Joshua <joshuawallace800@gmail.com>
2019-02-22 16:05:30 -0500

    removed unnecessary print statement

--- test/test_data_processing.py
+++ test/test_data_processing.py
@@ -6,7 +6,6 @@ This tests classes and methods in the data_processing.py file.
 import unittest
 import sys, os
 sys.path.insert(1,os.path.abspath('../src'))
-print(os.path.abspath('../src'))
 import data_processing as dproc
 import numpy as np
 
