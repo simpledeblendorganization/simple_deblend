426603c3f3e507ebea5b16d0e72787973256c28e
wally800 <joshuajw@princeton.edu>
2019-03-01 15:03:49 -0500

    added rr lyrae test

--- test/test_data_processing.py
+++ test/test_data_processing.py
@@ -25,7 +25,7 @@ class test_data_processing_init(unittest.TestCase):
         self.assertIsInstance(dproc.periodsearch_results('1'),
                                   dproc.periodsearch_results)
 
-class test_data_processing_run(unittest.TestCase):
+class test_data_processing_run_sinusoidal_single_signal(unittest.TestCase):
 
     def setUp(self):
         # random number
@@ -45,42 +45,6 @@ class test_data_processing_run(unittest.TestCase):
                                   [sigma1]*sample_len_1,0.5,0,'object2')
 
 
-        # another random number
-        rand2 = np.random.RandomState(seed=1847)
-        
-        ### col_b, more full test with some blending
-        self.col_b = dproc.lc_collection_for_processing(5.,n_control_workers=1)#2)
-        sample_len_2 = 5000
-        t2 = np.linspace(0,1000,sample_len_2)
-        self.omega1 = 1.
-        phi1 = 0.
-        self.omega2 = .582927
-        self.omega3 = 1.2023432
-        phi2 = .3422982
-        sigma1 = .01
-        self.col_b.add_object(t2,3. + np.sin(self.omega1*t2+phi1) +
-                                  .02*np.sin(self.omega2*t2+phi2) +
-                                  sigma1*rand2.randn(sample_len_2),
-                                  [sigma1]*sample_len_2,
-                                  0.,0.,'o1')
-        sigma2 = .03
-        self.col_b.add_object(t2,3. + .12*np.sin(self.omega3*t2+phi1) +
-                                  .5*np.sin(self.omega2*t2+phi2) +
-                                  sigma2*rand2.randn(sample_len_2),
-                                  [sigma2]*sample_len_2,
-                                  0.,0.0001,'o2')
-        sigma3 = .05
-        self.col_b.add_object(t2,3.+.94*np.sin(self.omega2*t2+phi2) +
-                                  sigma3*rand2.randn(sample_len_2),
-                                  [sigma3]*sample_len_2,3.,3.99,'o3')
-        sigma4 = .01
-        self.col_b.add_object(t2,[3.5]*sample_len_2,
-                                  [sigma4]*sample_len_2,2.,1.5,'o4')
-        sigma5 = .07
-        self.col_b.add_object(t2,2. + 3.*np.sin(self.omega2*t2+phi2) +
-                                  .5*np.sin(self.omega1*t2+phi1) +
-                                  sigma5*rand2.randn(sample_len_2),
-                                  [sigma5]*sample_len_2,10000,10000,'o5')
 
         ### col_c, test with some blending
         rand3 = np.random.RandomState(seed=1847)
@@ -214,6 +178,48 @@ class test_data_processing_run(unittest.TestCase):
 
 
 
+"""
+class test_data_processing_run_sinusoidal_multiple_signals(unittest.TestCase):
+
+    def setUp(self):
+        # another random number
+        rand2 = np.random.RandomState(seed=1847)
+        
+        ### col_b, more full test with some blending
+        self.col_b = dproc.lc_collection_for_processing(5.,n_control_workers=1)#2)
+        sample_len_2 = 5000
+        t2 = np.linspace(0,1000,sample_len_2)
+        self.omega1 = 1.
+        phi1 = 0.
+        self.omega2 = .582927
+        self.omega3 = 1.2023432
+        phi2 = .3422982
+        sigma1 = .01
+        self.col_b.add_object(t2,3. + np.sin(self.omega1*t2+phi1) +
+                                  .02*np.sin(self.omega2*t2+phi2) +
+                                  sigma1*rand2.randn(sample_len_2),
+                                  [sigma1]*sample_len_2,
+                                  0.,0.,'o1')
+        sigma2 = .03
+        self.col_b.add_object(t2,3. + .12*np.sin(self.omega3*t2+phi1) +
+                                  .5*np.sin(self.omega2*t2+phi2) +
+                                  sigma2*rand2.randn(sample_len_2),
+                                  [sigma2]*sample_len_2,
+                                  0.,0.0001,'o2')
+        sigma3 = .05
+        self.col_b.add_object(t2,3.+.94*np.sin(self.omega2*t2+phi2) +
+                                  sigma3*rand2.randn(sample_len_2),
+                                  [sigma3]*sample_len_2,3.,3.99,'o3')
+        sigma4 = .01
+        self.col_b.add_object(t2,[3.5]*sample_len_2,
+                                  [sigma4]*sample_len_2,2.,1.5,'o4')
+        sigma5 = .07
+        self.col_b.add_object(t2,2. + 3.*np.sin(self.omega2*t2+phi2) +
+                                  .5*np.sin(self.omega1*t2+phi1) +
+                                  sigma5*rand2.randn(sample_len_2),
+                                  [sigma5]*sample_len_2,10000,10000,'o5')
+
+
         
     def _test_blended_ls_run(self):
         self.col_b.run_ls(startp=2.,endp=11.,stepsize=1e-5,autofreq=False)
@@ -241,8 +247,58 @@ class test_data_processing_run(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.col_b.results['o4']['LS']
         self.assertEqual(len(self.col_b.results['o4'].keys()),0)
+"""
 
+
+class test_data_processing_run_rrlyrae_signal(unittest.TestCase):
+
+    def setUp(self):
+
+        self.rr_period = 0.490751
+        self.fourier_amp = np.array([-0.09340915, -0.28991142, -0.14849184,
+                                     -0.1043798 , -0.06841426,-0.04780541, 
+                                     -0.02871969, -0.01738949, -0.01293884,  
+                                     0.00956898,   0.00611337,  0.00324416])
+        self.fourier_phase=np.array([ 0.12177694, -0.56953169, -0.34281352,  
+                                      0.21209113,  0.70671323, 1.31876177,  
+                                      1.89734647,  2.28429027,  2.66283256,  
+                                      0.10880013,  0.75884711,  1.30690127])
+        time_length = 3000
+        t = np.linspace(0,75.21,time_length)
+        phase = t/self.rr_period
+        phase = phase - np.floor(phase)
+        fseries = np.array([self.fourier_amp[i]*np.cos(2.0*np.pi*i*phase + 
+                                              self.fourier_phase[i])
+                   for i in range(len(self.fourier_amp))])
+
+        # Then add this to the mags
+        rand = np.random.RandomState(seed=1820)
+        sigma1 = .01
+        mags_1 = 13.5 + sigma1*rand.randn(time_length)
+        for fo in fseries:
+            mags_1 += fo
+
+        sigma2 = .02
+        mags_2 = 16. + sigma2*rand.randn(time_length)
+        for fo in fseries:
+            mags_2 += .1*fo
+
+        self.col = dproc.lc_collection_for_processing(5.,n_control_workers=1)
+        self.col.add_object(t,mags_1,[sigma1]*time_length,0.,0.,'rr1')
+        self.col.add_object(t,mags_2,[sigma2]*time_length,1.,1.6,'rr2')
+
+
+    def test_rrblend(self):
+        # Test an RRab and a blend
+        self.col.run_ls(startp=.2,endp=1.,stepsize=1e-4,autofreq=False)#True)
+
+        self.assertEqual(len(self.col.results['rr1']['LS'].good_periods_info),1)
+        self.assertEqual(len(self.col.results['rr1']['LS'].blends_info),0)
+        self.assertAlmostEqual(self.col.results['rr1']['LS'].good_periods_info[0]['lsp_dict']['bestperiod'],self.rr_period,places=2)
+        self.assertEqual(len(self.col.results['rr2']['LS'].good_periods_info),0)
+        self.assertEqual(len(self.col.results['rr2']['LS'].blends_info),1)
+        self.assertEqual(self.col.results['rr2']['LS'].blends_info[0]['ID_of_blend'],'rr1')
+        
 
 if __name__ == '__main__':
     unittest.main()
-
