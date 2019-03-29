f056133a9c5b0cf0a51972fc99dbd137421b60ed
joshuawallace <joshuwallace800@gmail.com>
2019-03-29 19:30:09 -0400

    starting to implement main example

--- example/run_personal.py
+++ example/run_personal.py
@@ -11,13 +11,15 @@ import sys, os
 sys.path.insert(1,os.path.abspath('../src'))
 import data_processing as dproc
 import glob
+import pickle
 
 
-if len(sys.argv) != 4:
-    print("Expecting n arguments:")
+if len(sys.argv) != 5:
+    print("Expecting 4 arguments:")
     print(" - path to input light curve files")
     print(" - neighbor inclusion radius")
     print(" - number of control workers")
+    print(" - file with x,y position information")
     exit(-1)
 
 
@@ -31,19 +33,65 @@ def get_input_light_curves(path_to_input_files):
     input_files = glob.glob(path_to_input_files + "/*")
     print("Number of input files: " + str(len(input_files)))
 
+    return_dict = {}
+    number_skipped = 0
 
-    # return format: an iterable collection of (t,lc,err) containers
-    return None
+    for f in input_files:
+        t, lc, err = pickle.load(open(f,"rb"),encoding='latin')
+        if len(t) < 800:
+            number_skipped += 1
+            continue
+        return_dict[f.split("/")[-1].split("_")[0]] = \
+            (t,lc,err)
+            
+    print("Number skipped for too few points: " + str(number_skipped))
+
+
+    # return format: a dictionary of form ID:(t,lc,err) 
+    return return_dict
+
+
+def get_xy(file_xy):
+    '''
+    This can be modified as desired to allow for arbitrary input,
+    as long as the output format matches what is here.
+    '''
+    
+    with open(file_xy,'r') as f:
+        lines = f.readlines()
+
+    return_dict = {}
+    for line in lines:
+        s = line.split()
+        print(s[0])
+        if float(s[3]) < 19.:
+            return_dict[s[0]] = (float(s[-2]),float(s[-1]))
+
+    # return format: a ditionary of form ID:(x,y)
+    return return_dict
 
 
 def main():
     path_to_input_files = sys.argv[1]
     neighbor_radius = float(sys.argv[2])
     n_control_workers = int(sys.argv[3])
+    file_xy = sys.argv[4]
 
+    # Get the light curves
+    lcs = get_input_light_curves(path_to_input_files)
 
+    # Get xy positions
+    xy = get_xy(file_xy)
+
+    # Initialize the object to be ran
     col = dproc.lc_collection_for_processing(neighbor_radius,
                                              n_control_workers=n_control_workers)
+
+    # Add objects
+    for ID in lcs.keys():
+        lc = lcs[ID]
+        this_xy = xy[ID]
+        col.add_object(lc[0],lc[1],lc[2],this_xy[0],this_xy[1],ID)
 
     
 
