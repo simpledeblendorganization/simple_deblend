070ffb5f2e34b4a571d9ca107174d03802c67942
wally800 <joshuajw@princeton.edu>
2019-04-02 20:35:42 -0400

    merging

---
+++ example/personal_thresholds.py
@@ -0,0 +1,9 @@
+'''personal_thresholds.py - Joshua Wallace - Apr 2019
+
+This stores the SNR threshold functions determined to work for my
+M4 work.
+
+'''
+
+
+# Remember to make it return single-valued value
