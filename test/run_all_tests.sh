c202be7c035a7e7b374e8e903489bdb3e7db640c
joshuawallace <joshuwallace800@gmail.com>
2019-04-12 18:29:56 -0400

    updated test run script to delete output files at end

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -11,3 +11,8 @@ python test_data_processing_ls.py
 python test_data_processing_pdm.py
 
 python test_data_processing_bls.py
+
+rm -f ps_{c,d,e,o}{1,2,3,4,5}_*_{blends,goodperiod}.pkl
+rm -f ps_object{1,2}_*_{blends,goodperiod}.pkl
+rm -f ps_rr{1,2}_*_{blends,goodperiod}.pkl
+rm -f ps_s_*_{blends,goodperiod}.pkl
