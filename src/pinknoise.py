a7e5c1215c938255259ccaf543cefa49768dccc9
joshuawallace <joshuwallace800@gmail.com>
2019-04-12 20:00:21 -0400

    got full pinknoise up and going I think

--- src/pinknoise.py
+++ src/pinknoise.py
@@ -31,38 +31,6 @@ def weighted_rms(vals,errs):
 
 
 
-"""
-def ____binnedrms(times,mags,errs,transit_duration):
-
-
-    # Bin the magnitudes
-    mintime = times.min()
-    n_bins = np.ceil( (times.max()-mintime)/transit_duration)
-
-    binned_mags = []
-    for i in range(n_bins):
-        binned_mags.append([])
-
-    for t,m,e in zip(times,mags,errs):
-        binned_mags[np.floor((t-mintime)/transit_duration)] = (m,e)
-
-    # Calculate average magnitude in each bin
-    avg_mags = []
-    avg_mags_weightperbin = []
-    for i in range(len(binned_mags)):
-        if binned_mags[i]:
-            this_bin_mags, this_bin_errs = zip(*binned_mags[i])
-            avg_mags.append(np.average(this_bin_mags,weights=1/np.power(this_bin_errs,2)))
-            avg_mags_weightperbin.append(np.sum(1./np.power(this_bin_errs,2)))
-
-        
-
-    avesum3 = np.sum(np.power(binsig,2))
-    rmsbinthy = np.sqrt(avesum3 / n)
-
-    return weighted_rms(np.array(avg_mags),np.array(avg_mags_weightperbin),
-                        errsasweights=True)
-"""
 
 
 def dmax(a,b):
@@ -77,34 +45,6 @@ def dmin(a,b):
     else:
         return b
 
-
-"""
-def getrms(times,mags,errs):
-  if len(times) > 0:
-      avesum1 = 0.
-      avesum2 = 0.
-      avesum3 = 0.
-
-      for i in range(len(times)):
-          avesum1 += mag[i]
-          avesum2 += mag[i]*mag[i]
-          avesum3 += sig[i]*sig[i]
-
-      if n > 0.:
-	  ave = avesum1 / float(len(avesum1))
-	  rmsthy = np.sqrt(avesum3 / float(len(avesum3)))
-	  rmsval = np.sqrt((avesum2 / float(len(avesum2))) - (ave*ave))
-
-      else:
-	  rmsthy = -1.;
-	  rmsval = -1.;
-
-  else:
-      rmsthy = -1.
-      rmsval = -1.
-
-  return(rmsval, rmsthy)
-"""
 
 
 
@@ -205,10 +145,8 @@ def redwhitenoise_calc(times,mags,errs,period,q):
 
     rednoise_squared = rmsbinval**2 - rmsbinval_expected**2
     if rednoise_squared > 0.:
-        print("Red noise: " + str(np.sqrt(rednoise_squared)))
         return (rmsval, np.sqrt(rednoise_squared))
     else:
-        print("Red noise:  0.")
         return (rmsval, 0.)
 
 
@@ -238,13 +176,13 @@ def pinknoise_calc(times,mags,errs,period,transitduration,depth,
         # eebls.f returns +ve transit depth for fluxes
         # so add it in to get "flat" light curve
         corrected_mags[transitindices] = (
-            corrected_mags[transitindices] + depth
+            corrected_mags[transitindices] - depth
             )
     else:
         # eebls.f returns -ve transit depth for magnitudes
         # so add it in to get "flat" light curve
         corrected_mags[transitindices] = (
-            corrected_mags[transitindices] + depth
+            corrected_mags[transitindices] - depth
             )
 
     
@@ -260,12 +198,137 @@ def pinknoise_calc(times,mags,errs,period,transitduration,depth,
 
 if __name__ == "__main__":
     
-    # Print a basic one
+    ################
+    # Test a basic one
+    print("First test, expecting WN 0.005, RN 0, SPN 0")
     t1 = np.linspace(0,100,5000)
     rand1 = np.random.RandomState(seed=1844)
 
     mags1 = 13. + 0.005*rand1.randn(5000)
     errs1 = np.array([0.005]*5000)
 
-    print(pinknoise_calc(t1,mags1,errs1,3.4,.1,0.,int(.1*5000),
-                         8.2,int(100/3.4)))
+    spn = pinknoise_calc(t1,mags1,errs1,3.4,.1,0.,int(.1*5000),
+                         8.2,int(100/3.4))
+    print("SPN: " + str(spn))
+
+
+    #################
+    # Test a basic one w/ transit
+    print("\nSecond test, expecting WN 0.007, RN 0, SPN 406")
+    t2 = np.linspace(0,100,8000)
+    rand2 = np.random.RandomState(seed=1847)
+
+    q2 = 0.07
+    depth2 = 0.12
+    sigma2 = 0.007
+    mags2 = 13. + sigma2*rand1.randn(8000)
+    errs2 = np.array([sigma2]*8000)
+    per2 = 5.7
+    npt_t2 = 0
+    epoch2 = 1.
+    for i in range(len(t2)):
+        phase = (t2[i] - epoch2)/per2 - np.floor((t2[i] - epoch2)/per2)
+        if phase < q2/2. or phase > (1. - q2/2.):
+            npt_t2 += 1
+            mags2[i] += depth2
+    spn = pinknoise_calc(t2,mags2,errs2,per2,q2,depth2,npt_t2,
+                         epoch2,18)
+    print("SPN: " + str(spn))
+
+
+    ###################
+    # Test a sawtooth with transit
+    print("\nThird test, expecting WN 0.577, RN .3333, SPN 1.1997")
+    t3 = np.linspace(0,100,8000)
+    rand3 = np.random.RandomState(seed=1848)
+
+    # straight line stddev = 0.5774080071035848
+    # rmsval * rmsbinval_thy / rmsval_thy
+    # 0.01 *  0.01/sqrt(npointsperbin, 25 or so)               /0.01
+    #rednoise_squared = rmsbinval**2 - (0.01/5)**2
+    # expect .333396
+
+    q3 = 0.05
+    depth3 = 0.1
+    sigma3 = 0.01
+    per3 = 2.*np.pi
+    from scipy.signal import sawtooth
+    saw3_amp = 1.
+    mags3 = 10. + saw3_amp*sawtooth(t3) + sigma3*rand1.randn(8000)
+    errs3 = np.array([sigma3]*8000)
+    npt_t3 = 0
+    epoch3 = 3.3
+    for i in range(len(t3)):
+        phase = (t3[i] - epoch3)/per3 - np.floor((t3[i] - epoch3)/per3)
+        if phase < q3/2. or phase > (1. - q3/2.):
+            npt_t3 += 1
+            mags3[i] += depth3
+    spn = pinknoise_calc(t3,mags3,errs3,per3,q3,depth3,npt_t3,
+                         epoch3,16)
+    print("SPN: " + str(spn))
+
+
+
+    ###################
+    # Test a sawtooth with transit, smaller sawtooth amplitude
+    print("\nFourth test, expecting WN 0.288, RN ?, SPN ?")
+    t4 = np.linspace(0,100,8000)
+    rand4 = np.random.RandomState(seed=1848)
+
+    # straight line stddev = 0.288704
+    # rmsval * rmsbinval_thy / rmsval_thy
+    # 0.01 *  0.01/sqrt(npointsperbin, 25 or so)               /0.01
+    #rednoise_squared = rmsbinval**2 - (0.01/5)**2
+    # expect .333396
+
+    q4 = 0.05
+    depth4 = 0.1
+    sigma4 = 0.01
+    per4 = 2.*np.pi
+    from scipy.signal import sawtooth
+    saw4_amp = 0.5
+    mags4 = 10. + saw4_amp*sawtooth(t4) + sigma4*rand1.randn(8000)
+    errs4 = np.array([sigma4]*8000)
+    npt_t4 = 0
+    epoch4 = 3.3
+    for i in range(len(t4)):
+        phase = (t4[i] - epoch4)/per4 - np.floor((t4[i] - epoch4)/per4)
+        if phase < q4/2. or phase > (1. - q4/2.):
+            npt_t4 += 1
+            mags4[i] += depth4
+    spn = pinknoise_calc(t4,mags4,errs4,per4,q4,depth4,npt_t4,
+                         epoch4,16)
+    print("SPN: " + str(spn))
+
+
+
+    ###################
+    # Test a sawtooth with transit, smaller sawtooth amplitude
+    print("\nFifth test, expecting WN 0.0577, RN ?, SPN ?")
+    t4 = np.linspace(0,100,8000)
+    rand4 = np.random.RandomState(seed=1848)
+
+    # straight line stddev = 0.0577408007103584
+    # rmsval * rmsbinval_thy / rmsval_thy
+    # 0.01 *  0.01/sqrt(npointsperbin, 25 or so)               /0.01
+    #rednoise_squared = rmsbinval**2 - (0.01/5)**2
+    # expect .333396
+
+    q4 = 0.05
+    depth4 = 0.1
+    sigma4 = 0.01
+    per4 = 2.*np.pi
+    from scipy.signal import sawtooth
+    saw4_amp = 0.1
+    mags4 = 10. + saw4_amp*sawtooth(t4) + sigma4*rand1.randn(8000)
+    errs4 = np.array([sigma4]*8000)
+    npt_t4 = 0
+    epoch4 = 3.3
+    for i in range(len(t4)):
+        phase = (t4[i] - epoch4)/per4 - np.floor((t4[i] - epoch4)/per4)
+        if phase < q4/2. or phase > (1. - q4/2.):
+            npt_t4 += 1
+            mags4[i] += depth4
+    spn = pinknoise_calc(t4,mags4,errs4,per4,q4,depth4,npt_t4,
+                         epoch4,16)
+    print("SPN: " + str(spn))
